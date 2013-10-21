ec67ddc5d21afde790ede279e80614475cbc02af
Murtaught <murtaught@gmail.com>
2013-10-21 12:29:24 +0400

    Cleaned code up a bit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
 string window_title = "Sartroscope show image";
@@ -12,60 +12,53 @@ string output_filename;
 
 // From http://www-personal.umich.edu/~shameem/haarcascade_eye.html
 string eyes_cascade_name = "haarcascade_eye.xml";
-string face_cascade_name = "haarcascade_frontalface_alt.xml";
-string nose_cascade_name = "haarcascade_nose.xml";
 
-cv::CascadeClassifier face_cascade;
 cv::CascadeClassifier eyes_cascade;
-cv::CascadeClassifier nose_cascade;
 cv::RNG rng(12345);
 
-void detectAndDisplay(cv::Mat frame, cv::Mat background)
+// frame and background expected to be monochrome
+void process(cv::Mat frame, cv::Mat background)
 {
-    // prepare given image for eye detection
-    cv::Mat detection_frame;
-    cv::cvtColor(frame, detection_frame, CV_BGR2GRAY);
-    cv::equalizeHist(detection_frame, detection_frame);
-
     // prepare result image
-    cv::Mat grayscale_frame;
-    cv::cvtColor(frame, grayscale_frame, CV_BGR2GRAY);
+    cv::Mat result = frame.clone();
 
     // blend with background
-    cv::cvtColor(background, background, CV_BGR2GRAY);
     cv::Mat backgr_part = background(cv::Rect(0, 0, frame.cols, frame.rows));
-    cv::addWeighted(grayscale_frame, 0.8, backgr_part, 0.2, 0.0, grayscale_frame);
+    cv::addWeighted(result, 0.8, backgr_part, 0.2, 0.0, result);
 
     // detect eyes
     std::vector<cv::Rect> eyes;
     eyes_cascade.detectMultiScale(frame, eyes, 1.2, 2, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
 
+    // draw scary eyes
     for(int i = 0; i < eyes.size(); i++)
     {
-       cv::Point center(eyes[i].x + eyes[i].width*0.5, eyes[i].y + eyes[i].height*0.5);
-       int radius = cvRound( (eyes[i].width + eyes[i].height)*0.10 );
-       cv::circle(grayscale_frame, center, radius, cv::Scalar( 255, 255, 255 ), -1, 8, 0 );
+       cv::Point center(eyes[i].x + eyes[i].width  * 0.5,
+                        eyes[i].y + eyes[i].height * 0.5);
+       int radius = cvRound( (eyes[i].width + eyes[i].height) * 0.1 );
+
+       // draw filled (-1 arg) white circle with center at 'center'
+       cv::circle(result, center, radius, cv::Scalar(255, 255, 255), -1, 8, 0 );
     }
 
-    cv::Mat gaussian_noise_frame = grayscale_frame.clone();
-    cv::randn(gaussian_noise_frame, 128, 30);
+    // add some random noise
+    cv::Mat noise(result.size(), result.type());
+    cv::randn(noise, 128, 30);
+    cv::addWeighted(result, 0.8, noise, 0.2, 0.0, result);
 
-    cv::Mat noised_grframe;
-    cv::addWeighted(grayscale_frame, 0.8, gaussian_noise_frame, 0.2, 0.0, noised_grframe);
-
-    cv::Mat smoothed_ngrframe;
-    cv::GaussianBlur(noised_grframe, smoothed_ngrframe, cv::Size(3, 3), 0, 0);
+    // add some blur
+    cv::GaussianBlur(result, result, cv::Size(3, 3), 0, 0);
 
     if ( output_filename == "" )
     {
-        cv::imshow(window_title, smoothed_ngrframe);
+        cv::imshow(window_title, result);
     }
     else
     {
         // write result to file
         cout << "Writing to \"" << output_filename << "\"... ";
 
-        cv::imwrite(output_filename, smoothed_ngrframe);
+        cv::imwrite(output_filename, result);
         cout << "OK!" << endl;
     }
 }
@@ -89,7 +82,7 @@ int main(int argc, char** argv)
     }
 
     // Load background file
-    cv::Mat background = cv::imread(argv[2]);
+    cv::Mat background = cv::imread(argv[2], CV_LOAD_IMAGE_GRAYSCALE);
     if ( !background.data )
     {
         cout << "No background data" << endl;
@@ -102,14 +95,14 @@ int main(int argc, char** argv)
         output_filename = "";
 
         // Load image file
-        cv::Mat image = cv::imread(argv[1]);
+        cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);
         if( !image.data )
         {
             cout << "No image data" << endl;
             return -2;
         }
 
-        detectAndDisplay(image, background);
+        process(image, background);
 
         // If we show result in a window wait for a keypress
         // else exit immediately
@@ -131,7 +124,7 @@ int main(int argc, char** argv)
 
                 if ( !frame.empty() )
                 {
-                    detectAndDisplay(frame, background);
+                    process(frame, background);
                 }
                 else
                 {
